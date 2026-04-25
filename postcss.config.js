8637d36290e92898f3f59089f122eb40bd4de4ef
Your Name <you@example.com>
2026-04-26 03:51:41 +0800

    Fix postcss conflict and remove broken HTML file

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   plugins: {
-<<<<<<< HEAD
     tailwindcss: {},
     autoprefixer: {},
-=======
-    '@tailwindcss/postcss': {},
->>>>>>> 8a6c23ee5e29688621e7a75a5c95fff7df3e92f1
   },
 }
